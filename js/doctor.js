a35c9d6a2e6c406997d679505038719d8677088c
Margie Nevarez <margie.ynm@gmail.com>
2017-01-06 11:52:47 -0800

    output additional information for doctor

--- js/doctor.js
+++ js/doctor.js
@@ -6,10 +6,19 @@ function Doctor () {
 
 Doctor.prototype.getDoctor = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
-    $('.output').text("Results for your " + medicalIssue + " are the following: ")
+    $('.output').text("<h4>" + "Results for your " + medicalIssue + " are the following: " + "</h4>")
     response.data.forEach(function(doctor) {
       $('.result').append(`
-        <li>${doctor.profile.first_name} ${doctor.profile.last_name}</li>
+        <div class = "doc-info"
+          <ul>
+            <li>${doctor.profile.first_name}, ${doctor.profile.last_name}
+              <p><img src= "${doctor.profile.image_url}"</p>
+              <p>Phone number: ${doctor.practices[0].phones[0].number}</p>
+              <p>Street Address: ${doctor.practices[0].visit_address.street}</p>
+              <p>${doctor.profile.bio}</p>
+            </li>
+          </ul>
+        </div>
       `)
     })
       console.log(response);
